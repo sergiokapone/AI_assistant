fdf70e790b52b3b280ca7d2d89770f9573238b8c
sergiokapone <sergiy.ponomarenko@gmail.com>
2023-12-23 10:57:34 +0200

    add client

---
+++ client/src/app.py
@@ -0,0 +1,18 @@
+# app.py
+
+import streamlit as st
+
+def main():
+    st.title("Пример приложения с Streamlit")
+
+    # Добавление виджетов
+    name = st.text_input("Введите ваше имя", "Имя")
+    submit_button = st.button("Отправить")
+
+    # Обработка событий
+    if submit_button:
+        st.write(f"Привет, {name}!")
+
+if __name__ == "__main__":
+    main()
+
