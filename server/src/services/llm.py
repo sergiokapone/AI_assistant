6d2ff7932af8feae12cc702e49d4d68fe04f0cd4
Denys Filichkin <denys.philichkin@gmail.com>
2023-12-29 19:14:15 +0200

    Update llm.py

--- server/src/services/llm.py
+++ server/src/services/llm.py
@@ -10,7 +10,7 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain.docstore.document import Document
 
 # load the document and split it into chunk
-loader = TextLoader("D:/Alex/Documents/TestingLLM/test_file.txt")
+loader = TextLoader("server/src/services/output.txt")
 documents = loader.load()
 
 template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Use three sentences maximum. Keep the answer as concise as possible. Always say "thanks for asking!" at the end of the answer. 
@@ -19,7 +19,7 @@ Question: {question}
 Helpful Answer:"""
 
 prompt = PromptTemplate.from_template(template)
-#prompt = hub.pull("rlm/rag-prompt")
+# prompt = hub.pull("rlm/rag-prompt")
 
 # split it into chunks
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
@@ -34,17 +34,20 @@ API_KEY = "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"
 llm_id = "databricks/dolly-v2-3b"
 transformer_id = "sentence-transformers/all-MiniLM-L6-v2"
 
-llm = HuggingFaceHub(repo_id=llm_id, huggingfacehub_api_token=API_KEY, model_kwargs={"temperature":0.2, "max_length":64})
+llm = HuggingFaceHub(repo_id=llm_id, huggingfacehub_api_token=API_KEY,
+                     model_kwargs={"temperature": 0.2, "max_length": 64})
 retriever = db.as_retriever()
+
 
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
+
 chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | prompt
-    | llm
-    | StrOutputParser()
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
 )
 
 print("ready")
