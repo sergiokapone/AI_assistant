58b6da439d40672465b66ecb83694f5abbb10995
Denys Filichkin <denys.philichkin@gmail.com>
2023-12-30 12:26:04 +0200

    Update llm_vector_db.py

--- server/src/services/llm_vector_db.py
+++ server/src/services/llm_vector_db.py
@@ -4,22 +4,9 @@ from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddi
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
 
-from langchain.prompts import PromptTemplate
-from langchain.schema import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
-
-
 # load the document and split it into chunk
-loader = TextLoader("D:\PYTHON\DataScience\AI_assistant\server\src\services\output.txt", encoding='utf-8')
+loader = TextLoader("output.txt", encoding='utf-8')
 documents = loader.load()
-
-# template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Use three sentences maximum. Keep the answer as concise as possible. Always say "thanks for asking!" at the end of the answer.
-# {context}
-# Question: {question}
-# Helpful Answer:"""
-#
-# prompt = PromptTemplate.from_template(template)
-
 
 # split it into chunks
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
@@ -32,21 +19,4 @@ transformer_id = "sentence-transformers/all-MiniLM-L6-v2"
 retriever = db.as_retriever()
 
 
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
-
-
-chain = (
-        {"context": retriever | format_docs, "question": RunnablePassthrough()}
-        # | prompt
-        | StrOutputParser()
-)
-
 print("ready")
-# while True:
-#     query = input()
-#     if not query:
-#         break
-#     # query it
-#     response = chain.invoke(query)
-#     print(response)
