10ee811854fd24ab6a8e2d354c948c6ee5a456e0
Denys Filichkin <denys.philichkin@gmail.com>
2023-12-30 12:08:48 +0200

    save

--- server/src/services/llm_vector_db.py
+++ server/src/services/llm_vector_db.py
@@ -10,28 +10,25 @@ from langchain_core.runnables import RunnablePassthrough
 
 
 # load the document and split it into chunk
-loader = TextLoader("D:\PYTHON\DataScience\AI_assistant\server\src\services\output.txt")
+loader = TextLoader("D:\PYTHON\DataScience\AI_assistant\server\src\services\output.txt", encoding='utf-8')
 documents = loader.load()
 
-template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Use three sentences maximum. Keep the answer as concise as possible. Always say "thanks for asking!" at the end of the answer. 
-{context}
-Question: {question}
-Helpful Answer:"""
+# template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Use three sentences maximum. Keep the answer as concise as possible. Always say "thanks for asking!" at the end of the answer.
+# {context}
+# Question: {question}
+# Helpful Answer:"""
+#
+# prompt = PromptTemplate.from_template(template)
 
-prompt = PromptTemplate.from_template(template)
-# prompt = hub.pull("rlm/rag-prompt")
 
 # split it into chunks
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 docs = text_splitter.split_documents(documents)
 # create the open-source embedding function
 embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-
-# load it into Chroma
-db = Chroma.from_documents(docs, embedding_function)
-
+# load it into Chroma and save to disk
+db = Chroma.from_documents(docs, embedding_function, persist_directory="chroma_db")
 transformer_id = "sentence-transformers/all-MiniLM-L6-v2"
-
 retriever = db.as_retriever()
 
 
@@ -41,15 +38,15 @@ def format_docs(docs):
 
 chain = (
         {"context": retriever | format_docs, "question": RunnablePassthrough()}
-        | prompt
+        # | prompt
         | StrOutputParser()
 )
 
 print("ready")
-while True:
-    query = input()
-    if not query:
-        break
-    # query it
-    response = chain.invoke(query)
-    print(response)
+# while True:
+#     query = input()
+#     if not query:
+#         break
+#     # query it
+#     response = chain.invoke(query)
+#     print(response)
