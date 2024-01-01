00920428245dde1fa42d87afb9bd0e5308322b95
Denys Filichkin <denys.philichkin@gmail.com>
2024-01-01 17:05:15 +0200

    the last save on today

--- server/src/routes/upload.py
+++ server/src/routes/upload.py
@@ -1,5 +1,14 @@
-from fastapi import APIRouter
+from typing import List
+
+from fastapi import APIRouter, UploadFile, File
 from fastapi.security import HTTPBearer
+
+from server.src.schemas.upload import Response
 
 router = APIRouter(prefix="/upload", tags=["Upload PDF file"])
 security = HTTPBearer()
+
+
+@router.post("/", response_model=Response)
+async def upload_pdf(files: List[UploadFile] = File(...)):
+    pdf_paths = []
