b58b76f03bea2062bea964554ae7aae213768607
Denys Filichkin <denys.philichkin@gmail.com>
2024-01-01 15:26:26 +0200

    seva chromadb

--- server/src/services/extract_text_from_pdf.py
+++ server/src/services/extract_text_from_pdf.py
@@ -7,6 +7,7 @@ import uuid
 from typing import Union, List
 
 import chromadb
+from chromadb.config import Settings
 
 # import
 from langchain.document_loaders import TextLoader
@@ -23,19 +24,21 @@ def extract_text_from_pdf(pdf_sources: List[Union[str, bytes, tempfile.SpooledTe
 
         for page in reader.pages:
             _pdf_text += page.extract_text()
-
-        print(type(_pdf_text))
-
         # Очищаем текст от недопустимых символов и тегов
         cleaned_text_pdf = re.sub(r'[\d/.]', '', _pdf_text)
         # print(f"text in the var: {cleaned_text}")
 
         # Try Chroma Client
-        chroma_client = chromadb.Client()
-        metadata_options = {"hnsw:space": "cosine"}  # You can change this to "ip" or "cosine" if needed
+        # chroma_client = chromadb.Client()
+        chroma_client = chromadb.PersistentClient(path="chromadb", settings=Settings(allow_reset=True))
+        print(chroma_client.heartbeat())
+
+        # metadata_options = {"hnsw:space": "cosine"}  # You can change this to "ip" or "cosine" if needed
+        new_collection_persistent = chroma_client.create_collection(name="collection_name_persistent",
+                                                                    metadata={"hnsw:space": "cosine"})
         # This allows us to create a client that connects to the server
-        collection = chroma_client.create_collection(name="cleaned_text_pdf", metadata=metadata_options)
-        print(collection)
+        # collection = chroma_client.create_collection(name="cleaned_text_pdf", metadata=metadata_options)
+        print(new_collection_persistent)
 
         text_splitter = RecursiveCharacterTextSplitter(separators=["\n\n", "\n"], chunk_size=500, chunk_overlap=0)
         docs = text_splitter.split_text(cleaned_text_pdf)
@@ -43,8 +46,8 @@ def extract_text_from_pdf(pdf_sources: List[Union[str, bytes, tempfile.SpooledTe
         for doc in docs:
             uuid_name = uuid.uuid1()
             print("document for", uuid_name)
-            collection.add(ids=[str(uuid_name)], documents=doc)
-        print(chroma_client)
+            new_collection_persistent.add(ids=[str(uuid_name)], documents=doc)
+        print(new_collection_persistent)
 
 
 # C:/Users/User/Downloads/design-patterns-uk.pdf
