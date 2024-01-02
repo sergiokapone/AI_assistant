5f0f289ca5a020437a8a5dcb86885bf987017ffc
OOlexandr <oleksarkhumua@gmail.com>
2024-01-02 12:35:28 +0200

    individual chains

--- server/src/services/llmchain.py
+++ server/src/services/llmchain.py
@@ -19,7 +19,7 @@ Helpful Answer:"""
 
 prompt = PromptTemplate.from_template(template)
 
-template_no_context = """Answer the question at the end. Use three sentences maximum. Keep the answer as concise as possible.
+template_no_context = """Your name is AIS{user_id}. Answer the question at the end. Use three sentences maximum. Keep the answer as concise as possible.
 Question: {question}
 Helpful Answer:"""
 
@@ -37,9 +37,8 @@ class Chain:
         self.hostory = history  # NYI
         if context:
             self.retriever = context.as_retriever()
-        self.create_chain()
 
-    def format_docs(docs):
+    def format_docs(self, docs):
         return "\n\n".join(doc.page_content for doc in docs)
 
     def create_chain(self):
@@ -55,11 +54,15 @@ class Chain:
             )
         else:
             self.chain = (
-                {"question": RunnablePassthrough()}
+                {"user_id": self.get_current_user,
+                 "question": RunnablePassthrough(),}
                 | prompt_no_context
                 | self.llm
                 | StrOutputParser()
             )
+
+    def get_current_user(self, *args):
+        return str(self.current_user)
 
     def create_context(self):
         # Тут создается vectorstore self.context и retriever
@@ -69,5 +72,7 @@ class Chain:
     def add_context(self):
         pass  # Когда мы найдем способ добавлять новые документы в контекст он будет имплиментирован здесь.
 
-    def __call__(self, query):
+    def __call__(self, query, user_id):
+        self.current_user = user_id
+        self.create_chain()
         return self.chain.invoke(query)
