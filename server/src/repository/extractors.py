f8da41e86de39c275a5c70e5d2b6915034acf32d
Denys Filichkin <denys.philichkin@gmail.com>
2024-01-05 14:53:40 +0200

    save

--- server/src/repository/extractors.py
+++ server/src/repository/extractors.py
@@ -4,23 +4,22 @@ import uuid
 from typing import List, Union
 
 import PyPDF2
+from fastapi import requests
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import TextLoader
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from ..database.models import UploadedText, User
 
+
 # read only digital PDF book which more 1000 sings
 
 
-async def extract_text(path):
-    ...
-
-
 async def extract_text_from_pdf(
-    current_user: User,
-    pdf_sources: List[Union[str, bytes, tempfile.SpooledTemporaryFile]],
-    session: AsyncSession,
-    chroma_helper,
+        current_user: User,
+        pdf_sources: List[Union[str, bytes, tempfile.SpooledTemporaryFile]],
+        session: AsyncSession,
+        chroma_helper,
 ) -> None:
     global collection_name
     cleaned_text_pdf = ""
@@ -58,9 +57,60 @@ async def extract_text_from_pdf(
     await session.commit()
 
 
-async def extract_text_from_txt():
-    ...
+async def extract_text_from_txt(
+        current_user: User,
+        text_sources: List[Union[str, bytes, tempfile.SpooledTemporaryFile]],
+        session: AsyncSession,
+        chroma_helper,
+) -> None:
+    global collection_name
+    text = ""
+
+    for text_source in text_sources:
+        loader = TextLoader(text_source)
+        documents = loader.load()
+
+        for document in documents:
+            text += document.extract_text()
+
+    collection_name = f"collection_{current_user.id}"
+    new_collection_persistent = chroma_helper.get_or_create_collection(
+        name=collection_name, metadata={"hnsw:space": "cosine"}
+    )
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=["\n\n", "\n"], chunk_size=1000, chunk_overlap=20
+    )
+    docs = text_splitter.split_text(text)
+
+    for doc in docs:
+        uuid_name = uuid.uuid1()
+        # print("document for", uuid_name)
+        new_collection_persistent.add(ids=[str(uuid_name)], documents=doc)
+
+    uploaded_text = UploadedText(
+        user_id=current_user.id,
+        uploaded_text=text,
+    )
+
+    session.add(uploaded_text)
+    await session.commit()
 
 
-async def extract_subtitles_from_youtube():
-    ...
+async def extract_subtitles_from_youtube(
+        current_user: User,
+        subtitles_sources: List[Union[str, bytes, tempfile.SpooledTemporaryFile]],
+        session: AsyncSession,
+        chroma_helper,
+) -> None:
+    global collection_name
+    cleaned_subtitles = ""
+
+    response = requests.get("url")
+    if response.status_code == 200:
+        captions = response.json()
+        if captions.get('items'):
+            subtitle_url = captions['items'][0]['snippet']['url']
+            subtitle_text = requests.get(subtitle_url).text
+            return cleaned_subtitles
+    return ""
--- server/src/routes/upload_pdf.py
+++ server/src/routes/upload_pdf.py
@@ -5,19 +5,19 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from ..database.db_helper import db_helper
 from ..database.models import User
-from ..repository.extractors import extract_text_from_pdf
+from ..repository.extractors import extract_text_from_pdf, extract_text_from_txt, extract_subtitles_from_youtube
 from ..services.auth import auth_service
 from ..vector_db.chroma_init import get_chroma_client, initialize_chroma_client
 
-router = APIRouter(prefix="/upload_pdf", tags=["Upload file"])
+router = APIRouter(prefix="/upload", tags=["Upload file"])
 
 
-@router.post("/")
+@router.post("/pdf")
 async def upload_pdf(
-    current_user: User = Depends(auth_service.get_authenticated_user),
-    file: UploadFile = File(...),
-    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
-    chroma_helper: initialize_chroma_client = Depends(get_chroma_client),
+        current_user: User = Depends(auth_service.get_authenticated_user),
+        file: UploadFile = File(...),
+        session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+        chroma_helper: initialize_chroma_client = Depends(get_chroma_client),
 ):
     pdf_paths = []
 
@@ -32,3 +32,48 @@ async def upload_pdf(
     await extract_text_from_pdf(current_user, pdf_paths, session, chroma_helper)
 
     return {"pdf_paths": pdf_paths}
+
+
+@router.post("/text")
+async def text_from_txt_endpoint(
+        current_user: User = Depends(auth_service.get_authenticated_user),
+        file: UploadFile = File(...),
+        session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+        chroma_helper: initialize_chroma_client = Depends(get_chroma_client),
+):
+    texts_paths = []
+
+    target_folder = "uploads"
+
+    file_path = os.path.join(target_folder, file.filename)
+    # Сохраняем загруженный файл
+    with open(file.filename, 'w') as buffer:
+        buffer.write(await buffer.read().decode())
+        texts_paths.append(buffer.name)
+
+    # Вызываем функцию извлечения текста из TXT
+    text = await extract_text_from_txt(current_user, texts_paths, session, chroma_helper)
+
+    return {"texts_paths": texts_paths}
+
+
+@router.post("/subtitles")
+async def subtitles_from_youtube_endpoint(
+        current_user: User = Depends(auth_service.get_authenticated_user),
+        file: UploadFile = File(...),
+        session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+        chroma_helper: initialize_chroma_client = Depends(get_chroma_client),
+):
+    subtitles_paths = []
+
+    target_folder = "uploads"
+
+    file_path = os.path.join(target_folder, file.filename)
+
+    with open(file.filename, 'w') as buffer:
+        buffer.write(await buffer.read().decode())
+        subtitles_paths.append(buffer.name)
+
+    subtitles = await extract_subtitles_from_youtube(current_user, subtitles_paths, session, chroma_helper)
+
+    return {"subtitles_paths": subtitles_paths}
