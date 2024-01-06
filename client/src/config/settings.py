1f5c2bfce4617563b3a3a8d40ed39b6a3aa04db9
sergiokapone <sergiy.ponomarenko@gmail.com>
2024-01-07 00:04:30 +0200

    change client settings

--- client/src/config/settings.py
+++ client/src/config/settings.py
@@ -3,12 +3,12 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     base_url: str = "http://127.0.0.1:8000/api/v1/"
-    sign_in_url: str = base_url + "auth/login/"
     sign_up_url: str = base_url + "auth/signup/"
+    sign_in_url: str = base_url + "auth/login/"
+    log_out_url: str = base_url + "auth/logout/"
     chat_url: str = base_url + "chat"
     uload_file_url: str = base_url + "upload_pdf/"
     llm_selector_url: str = base_url + "llm_selector/"
-    log_out_url: str = base_url + "auth/logout/"
     get_history_url: str = base_url + "history/"
 
 
